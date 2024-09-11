5b547f963012601070d7162bdf965810d12d838c
abodkhe1 <ajitbodkhe2017@gmail.com>
2024-09-11 18:38:15 +0530

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://ajitbodkhe2017:bQ2wJLHRXP3Dcxq0@mymongodb.msv2t.mongodb.net/?retryWrites=true&w=majority&appName=mymongodb', {
+// MongoDB connection string (local or cloud)
+const connectionString = 'mongodb+srv://ajitbodkhe2017:bQ2wJLHRXP3Dcxq0@mymongodb.msv2t.mongodb.net/?retryWrites=true&w=majority&appName=mymongodb';
+// If you want to switch to local, you can use this line:
+// const connectionString = 'mongodb://localhost:27017/leaddatabase';
+
+// Connect to MongoDB
+mongoose.connect(connectionString, {
     useNewUrlParser: true,   // Parses MongoDB connection strings correctly
     useUnifiedTopology: true // Uses the new server discovery and monitoring engine
 })
-// mongoose.connect('mongodb://localhost:27017/leaddatabase', {
-//     useNewUrlParser: true,   // Parses MongoDB connection strings correctly
-//     useUnifiedTopology: true // Uses the new server discovery and monitoring engine
-// })
     .then(() => {
         console.log('Connected to MongoDB');
     })
